da786656b35585d96a393034488d8edb30a20f27
Mark Chan <lok.423@hotmail.com>
2018-08-17 12:17:44 +1200

    change to local database

--- src/chat-server.ts
+++ src/chat-server.ts
@@ -43,7 +43,8 @@ export class ChatServer {
     */
     private createMongodb(){
       this.mongoose = require('mongoose');
-      this.mongoose.connect('mongodb://heroku_m353r10c:l59avnkgmk6ugd64k5i1roe7sr@ds121262.mlab.com:21262/heroku_m353r10c', function (err) {
+      this.mongoose.connect('mongodb://localhost/chat', function (err) {
+      //this.mongoose.connect('mongodb://heroku_m353r10c:l59avnkgmk6ugd64k5i1roe7sr@ds121262.mlab.com:21262/heroku_m353r10c', function (err) {
 if (err) {
       console.log(err);
 }else {
