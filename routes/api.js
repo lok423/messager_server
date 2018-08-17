738feadc01d2e2bd77d69c6f09cbfcda9610de1f
Mark Chan <lok.423@hotmail.com>
2018-08-17 14:14:11 +1200

    no message

--- routes/api.js
+++ routes/api.js
@@ -56,13 +56,13 @@ router.post('/upload-file', function(req, res){
   console.log(__dirname);
 
   // store all uploads in the /uploads directory
-  form.uploadDir = path.join(__dirname, '/../public/my-files/');
+  //form.uploadDir = path.join(__dirname, '/../public/my-files/');
 
   // every time a file has been uploaded successfully,
   // rename it to it's orignal name
   form.on('file', function(field, file) {
     console.log("file_path", file.path);
-    fs.rename(file.path, path.join(form.uploadDir, file.name));
+    //fs.rename(file.path, path.join(form.uploadDir, file.name));
     _filename = file.name;
   });
 
